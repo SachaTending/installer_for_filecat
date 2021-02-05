43b27e6d229678d7bebda5306dedcc4faf32b6c2
romanin-rf <semina054@gmail.com>
2021-02-05 20:13:44 +0200

    v0.1-beta-1
    Add handler for sys.argv
    
    Co-Authored-By: SachaTending <65766775+SachaTending@users.noreply.github.com>

--- InstallFileCat.py
+++ InstallFileCat.py
@@ -17,6 +17,36 @@ try:
 	sg.one_line_progress_meter('Loading is lib "sys"', 5, count_libs, 'LoadLib')
 	import sys
 except:
-	ctypes.windll.user32.MessageBoxW(0, "Failed to load libraries", "Installer for FileCat", 64)
+	raise ImportError("Failed to load modules to work with")
+
+# Создание переменой
+if sys.platform == "win32":
+	Directory = str(os.getcwd())
+	UI = True
+else:
+	raise OSError("Your operating system is not supported")
 
 # Логика загрузки
+if sys.platform == "win32":
+	if len(sys.argv) > 1:
+		if "-D" in sys.argv:
+			try:
+				Directory = str(sys.argv[int(sys.argv.index("-D")) + 1])
+			except:
+				ctypes.windll.user32.MessageBoxW(0, "SyntaxError: The folder path is not specified correctly", "Install FileCat", 16)
+				raise SyntaxError("The folder path is not specified correctly")
+			if not(os.path.isabs(Directory)):
+				ctypes.windll.user32.MessageBoxW(0, "NotADirectoryError: The specified folder does not exist", "Install FileCat", 16)
+				raise NotADirectoryError("The specified folder does not exist")
+		if "-UI" in sys.argv:
+			try:
+				UI = bool(sys.argv[int(sys.argv.index("-UI")) + 1])
+			except:
+				ctypes.windll.user32.MessageBoxW(0, "ValueError: The parameter value is not specified correctly", "Install FileCat", 16)
+				raise ValueError("The parameter value is not specified correctly")
+	if UI:
+		pass
+	else:
+		pass
+else:
+	raise OSError("Your operating system is not supported")
