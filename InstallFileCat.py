bac1137ef57cb711956f501a7eda1119545fed0b
romanin-rf <semina054@gmail.com>
2021-02-06 11:00:30 +0200

    v0.1-beta-1
    Release version
    
    Co-Authored-By: SachaTending <65766775+SachaTending@users.noreply.github.com>

--- InstallFileCat.py
+++ InstallFileCat.py
@@ -50,88 +50,88 @@ if sys.platform == "win32":
 		count_stages = 9
 		# Загрузка update_data.json
 		try:
-			sg.one_line_progress_meter('Download is "update_api.json"', 0, count_stages, 'Install')
+			sg.one_line_progress_meter('Installing...', 0, count_stages, 'Install')
 			namefile_update_data = wget.download("https://raw.githubusercontent.com/romanin-rf/FileCat/master/update_api.json")
 		except:
-			sg.one_line_progress_meter('Download is "update_api.json"', count_stages, count_stages, 'Install')
+			sg.one_line_progress_meter('Installing...', count_stages, count_stages, 'Install')
 			ctypes.windll.user32.MessageBoxW(0, "ConnectionError: An error occurred while uploading the file", "Install FileCat", 16)
 			raise ConnectionError("An error occurred while uploading the file")
 
 		# Открытие update_data.json
 		try:
-			sg.one_line_progress_meter('Loading is "update_api.json"', 1, count_stages, 'Install')
+			sg.one_line_progress_meter('Installing...', 1, count_stages, 'Install')
 			with open(str(namefile_update_data)) as UpdateDataFile:
 				UpdateData = json.load(UpdateDataFile)
 		except:
-			sg.one_line_progress_meter('Loading is "update_api.json"', count_stages, count_stages, 'Install')
+			sg.one_line_progress_meter('Installing...', count_stages, count_stages, 'Install')
 			ctypes.windll.user32.MessageBoxW(0, "BlockingIOError: Couldn't get information from file", "Install FileCat", 16)
 			raise BlockingIOError("Couldn't get information from file")
 
 		# Удаление update_data.json
 		try:
-			sg.one_line_progress_meter('Delected is "update_api.json"', 2, count_stages, 'Install')
+			sg.one_line_progress_meter('Installing...', 2, count_stages, 'Install')
 			os.remove(str(os.getcwd()) + "\\" + str(namefile_update_data))
 		except:
 			ctypes.windll.user32.MessageBoxW(0, "Warning: Couldn't delete file", "Install FileCat", 16)
 
 		# Переход в директорию для установки
 		try:
-			sg.one_line_progress_meter('Changing the directory', 3, count_stages, 'Install')
+			sg.one_line_progress_meter('Installing...', 3, count_stages, 'Install')
 			os.chdir(Directory)
 		except:
-			sg.one_line_progress_meter('Changing the directory', count_stages, count_stages, 'Install')
+			sg.one_line_progress_meter('Installing...', count_stages, count_stages, 'Install')
 			ctypes.windll.user32.MessageBoxW(0, "IsADirectoryError: Couldn't navigate to the installation directory", "Install FileCat", 16)
 			raise IsADirectoryError("Couldn't navigate to the installation directory")
 
 		# Создание папки для распаковки
 		try:
-			sg.one_line_progress_meter('Create directory for install', 4, count_stages, 'Install')
+			sg.one_line_progress_meter('Installing...', 4, count_stages, 'Install')
 			namedir_for_install = "FileCat-v{0}".format(UpdateData["version"])
 			os.mkdir(namedir_for_install)
 		except:
-			sg.one_line_progress_meter('Create directory for install', count_stages, count_stages, 'Install')
+			sg.one_line_progress_meter('Installing...', count_stages, count_stages, 'Install')
 			ctypes.windll.user32.MessageBoxW(0, "PermissionError: Failed to create folder", "Install FileCat", 16)
 			raise PermissionError("Failed to create folder")
 
 		# Переход в папку для загрузки и установки
 		try:
-			sg.one_line_progress_meter('Changing the directory', 5, count_stages, 'Install')
+			sg.one_line_progress_meter('Installing...', 5, count_stages, 'Install')
 			os.chdir(namedir_for_install)
 		except:
-			sg.one_line_progress_meter('Changing the directory', count_stages, count_stages, 'Install')
+			sg.one_line_progress_meter('Installing...', count_stages, count_stages, 'Install')
 			ctypes.windll.user32.MessageBoxW(0, "IsADirectoryError: Couldn't navigate to the installation directory", "Install FileCat", 16)
 			raise IsADirectoryError("Couldn't navigate to the installation directory")
 
 		# Загрузка пакета .zip для распаковки
 		try:
-			sg.one_line_progress_meter('Download is FileCat.zip', 6, count_stages, 'Install')
+			sg.one_line_progress_meter('Installing...', 6, count_stages, 'Install')
 			namefile_release_zip = str(wget.download(str(UpdateData["url"])))
 		except:
-			sg.one_line_progress_meter('Download is FileCat.zip', count_stages, count_stages, 'Install')
+			sg.one_line_progress_meter('Installing...', count_stages, count_stages, 'Install')
 			ctypes.windll.user32.MessageBoxW(0, "ConnectionError: An error occurred while uploading the file", "Install FileCat", 16)
 			raise ConnectionError("An error occurred while uploading the file")
 
 		# Проверка .zip
 		try:
 			if zipfile.is_zipfile(namefile_release_zip) == False:
-				sg.one_line_progress_meter('Checking the file', count_stages, count_stages, 'Install')
+				sg.one_line_progress_meter('Installing...', count_stages, count_stages, 'Install')
 				ctypes.windll.user32.MessageBoxW(0, "FileNotFoundError: The file failed validation", "Install FileCat", 16)
 				raise FileNotFoundError("The file failed validation")
-			sg.one_line_progress_meter('Checking the file', 7, count_stages, 'Install')
+			sg.one_line_progress_meter('Installing...', 7, count_stages, 'Install')
 		except:
-			sg.one_line_progress_meter('Checking the file', count_stages, count_stages, 'Install')
+			sg.one_line_progress_meter('Installing...', count_stages, count_stages, 'Install')
 			ctypes.windll.user32.MessageBoxW(0, "FileNotFoundError: The file failed validation", "Install FileCat", 16)
 			raise FileNotFoundError("The file failed validation")
 
 		# Распаковка .zip
 		try:
-			sg.one_line_progress_meter('Unpacking', 8, count_stages, 'Install')
+			sg.one_line_progress_meter('Installing...', 8, count_stages, 'Install')
 			with zipfile.ZipFile(namefile_release_zip, 'r') as ReleaseFileForUpacking:
 				ReleaseFileForUpacking.extractall()
-			sg.one_line_progress_meter('Unpacking', count_stages, count_stages, 'Install')
+			sg.one_line_progress_meter('Installing...', count_stages, count_stages, 'Install')
 			ctypes.windll.user32.MessageBoxW(0, "Installation is complete", "Install FileCat", 64)
 		except:
-			sg.one_line_progress_meter('Unpacking', count_stages, count_stages, 'Install')
+			sg.one_line_progress_meter('Installing...', count_stages, count_stages, 'Install')
 			ctypes.windll.user32.MessageBoxW(0, "OSError: Failed to complete the installation", "Install FileCat", 16)
 			raise OSError("Failed to complete the installation")
 else:
